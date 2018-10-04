2065ba46f5666eed5e1d9dc4b5743f9d716d9011
Karl <badik76@gmail.Comgit>
2018-10-04 09:54:30 +0200

    Base

--- exo2/assets/script.js
+++ exo2/assets/script.js
@@ -1,4 +1,8 @@
 var lastName = "Doe";
 var firstName = "John";
 var city = "New-York";
-alert(lastName \n firstName \n city);
+alert("\Nom : " + lastName + "\n" + "\Prénom : " + firstName + "\n"+ "\Ville : " + city);
+var Lastname = "Omar";
+var Firstname = "Moulah";
+var Village = "Une grotte";
+alert("\Le Blaze : " + Firstname + "\n" + Lastname + "\n" + "\Bled de prédilection : " + Village)
--- exo2/script.js
+++
@@ -1,4 +0,0 @@
-var lastName = "Doe";
-var firstName = "John";
-var city = "New-York";
-alert(lastName \n firstName \n city);
