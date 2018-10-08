003ae97a6ed49799bbb39c9b812f4aee2cc38f14
Karl <badik76@gmail.Comgit>
2018-10-08 14:05:36 +0200

    Fin de l'exo4

--- exo4/assets/script.js
+++ exo4/assets/script.js
@@ -1,4 +1,16 @@
-var lastName = prompt("C'est quoi ton ptit nom chewychou ?" , "<Entrez votre nom ici>");
-var firstName = prompt("Et ton nom de famille, Darling c'est quoi ?" ,"<Pose ton nom ici !>");
-var city = prompt("Et pour finir, j'ai besoin de connaitre ta ville de résidence, Fiston." , "<Entrez votre ville ici>");
-alert("\Nom : " + lastName + "\n" + "\Prénom : " + firstName + "\n"+ "\Ville : " + city);
+var lastname = document.getElementById("lastname");
+var firstname = document.getElementById("firstname");
+var city = document.getElementById("city");
+var submit = document.getElementById("submit");
+
+submit.addEventListener("click", function(){
+  if (lastname.value !=="" && firstname.value !=="" && city.value !=="") {
+    if (/^[a-zA-Z]+$/.test(lastname.value + firstname.value + city.value)){
+      alert("\Nom : " + lastname.value + "\n" + "\Prénom : " + firstname.value + "\n"+ "\Ville : " + city.value);
+    } else {
+        alert("EPIC FAIL RECOMMENCE");
+      }
+  }  else {
+        alert("Case(s) vide(s)")
+    }
+  })
