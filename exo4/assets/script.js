f96b831d0263ff50bae62055493ee1950103002d
Karl <badik76@gmail.Comgit>
2018-10-08 10:02:21 +0200

    Exo4 - reste sécu à faire

---
+++ exo4/assets/script.js
@@ -0,0 +1,4 @@
+var lastName = prompt("C'est quoi ton ptit nom chewychou ?" , "<Entrez votre nom ici>");
+var firstName = prompt("Et ton nom de famille, Darling c'est quoi ?" ,"<Pose ton nom ici !>");
+var city = prompt("Et pour finir, j'ai besoin de connaitre ta ville de résidence, Fiston." , "<Entrez votre ville ici>");
+alert("\Nom : " + lastName + "\n" + "\Prénom : " + firstName + "\n"+ "\Ville : " + city);
