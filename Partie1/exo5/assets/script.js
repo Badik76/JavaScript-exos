1031d66a9e4856c3bba33bd703be7b7760ef9c62
Karl <badik76@gmail.Comgit>
2018-10-09 16:49:14 +0200

    MaJ

--- Partie1/exo5/assets/script.js
+++ Partie1/exo5/assets/script.js
@@ -5,7 +5,6 @@ var submit = document.getElementById("submit"); // lien avec le html
 function isFloat(n){ //fonction pour n'avoir que des chiffres décimales.
       return n != "" && !isNaN(n) && Math.round(n) != n;
   }
-
 submit.addEventListener("click", function(){ //début de la fonction submit
   if (isFloat(firstNumber.value.replace(",", ".") && secondNumber.value.replace(",", "."))) { //Test du isFloat + remplacement de la , par un .
         alert(Math.trunc(firstNumber.value.replace(",", ".")) * secondNumber.value.replace(",", ".")); //résultat si bon
