565af053ba707bcc1d36a94fe96196ff6c91625e
Karl <badik76@gmail.Comgit>
2018-10-09 11:29:36 +0200

    Fin exo 2

--- Partie2/exo2/assets/script.js
+++ Partie2/exo2/assets/script.js
@@ -1,8 +1,4 @@
-var lastName = "Doe";
-var firstName = "John";
-var city = "New-York";
-alert("\Nom : " + lastName + "\n" + "\Prénom : " + firstName + "\n"+ "\Ville : " + city);
-var Lastname = "Omar";
-var Firstname = "Moulah";
-var Village = "Une grotte";
-alert("\Le Blaze : " + Firstname + "\n" + Lastname + "\n" + "\Bled de prédilection : " + Village)
+var lastname = document.getElementById("lastname") //lien avec le html
+lastname.addEventListener("focusout",function() {
+  alert("Merci de votre participation " + this.value + ", Vous êtes bien mignon et bien portant, BRAVO !");
+});
