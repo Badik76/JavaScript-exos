6f3593489751f85a98c78990249b4683459f6c64
Karl <badik76@gmail.Comgit>
2018-10-10 10:22:02 +0200

    Exo1p2 down

--- Partie3/exo1/assets/script.js
+++ Partie3/exo1/assets/script.js
@@ -1,3 +1,7 @@
-var maVariable = "BOUMCHIKACHIKA";
-alert(maVariable);
-alert(maVariable);
+var image1 = document.getElementById("image1"); //lien avec le html et l'id image1
+image1.addEventListener("mouseover",function(){// définition de la fonction mouseover avec une application de style lors du passage de la souris.
+  image1.style.border = "3px solid red" // définition du style désiré
+}) ;//fin de la fonction mouseover
+image1.addEventListener("mouseout",function(){ //fonction mouseout pour revenir à la normale après passage de souris
+  image1.style.border = ""  //définition d'aucun style quand souris pas sur image.
+}) //fin de la fonction mouseout.
