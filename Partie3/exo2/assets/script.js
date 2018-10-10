cfc01048799dd66fa760f667213ca07f24092f50
Karl <badik76@gmail.Comgit>
2018-10-10 11:13:25 +0200

    Exo2 fini et commenté

--- Partie3/exo2/assets/script.js
+++ Partie3/exo2/assets/script.js
@@ -1,8 +1,15 @@
-var lastName = "Doe";
-var firstName = "John";
-var city = "New-York";
-alert("\Nom : " + lastName + "\n" + "\Prénom : " + firstName + "\n"+ "\Ville : " + city);
-var Lastname = "Omar";
-var Firstname = "Moulah";
-var Village = "Une grotte";
-alert("\Le Blaze : " + Firstname + "\n" + Lastname + "\n" + "\Bled de prédilection : " + Village)
+var links = document.links; //définition d'une variable pour référencer les href dans le html
+var text = document.getElementById("text") //lien entre div text et le js
+
+for(var i = 0; i < links.length; i++) { //définition d'une variable qui s'incrémente pour les href afin de trouver les deux
+  if (links[i].innerHTML=="Afficher"){// innerHTML va tester (==) et chercher afficher
+links[i].addEventListener("click",function(){ //fonction click associé au afficher
+text.style.display = "block" // le block text réaparé
+})//fin de fonction click afficher
+}//fin du if afficher
+if (links[i].innerHTML=="Masquer"){ // innerHTMLva tester et chercher Masquer
+  links[i].addEventListener("click",function(){  //fonction click associé au masqué
+text.style.display = "none"//le text sera caché.
+})//fin de fonction click masquer
+}//fin du if masquer
+} //fin de la boucle
