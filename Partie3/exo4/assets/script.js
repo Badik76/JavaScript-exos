91e973d661b03548b252f2ae547b1b391ab39b42
Karl <badik76@gmail.Comgit>
2018-10-11 11:09:52 +0200

    Fin Jasmine P1

--- Partie3/exo4/assets/script.js
+++ Partie3/exo4/assets/script.js
@@ -1,13 +1,14 @@
 var password = document.getElementById("password")//asso avec html id=password
 var confirmPassword = document.getElementById("confirmPassword")//asso avec html id=confirmPassword
-var button = document.getElementsByName("button")[0]
+var button = document.getElementsByName("button")[0] //asso html avec button + [0] car ByName
 
-button.addEventListener("click", function() {
-  if (password.value!=confirmPassword.value) {
-  password.style.border = "3px solid red"
-  confirmPassword.style.border = "3px solid red"
-    alert("Raté, c'est pas le même mot de passe !")
-} else {
-  password.style.border = "3px solid green"
-  confirmPassword.style.border = "3px solid green"
-}})
+button.addEventListener("click", function() { //lorsque l'on click sur le button, l'action se lance
+  if (password.value!=confirmPassword.value) { //vérification que les 2 passwords sont strictement pas (!=) indentique
+  password.style.border = "5px solid red" // si pas identique, changement style bordure red
+  confirmPassword.style.border = "5px solid red"// si pas identique, changement style bordure red
+    alert("Raté, c'est pas le même mot de passe !")//pop-up pour signaler l'erreur en plus.
+} else {//si jamais les deux pwr sont identiques alors
+  password.style.border = "5px solid green"//  identique, changement style bordure green
+  confirmPassword.style.border = "5px solid green"//  identique, changement style bordure gree
+}//fin du else
+})//fin de l'action lors d'un click.
